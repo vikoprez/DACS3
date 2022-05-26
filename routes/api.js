f8582883902cc05f1d2e8995c5062c58e5f775ae
vikopromotion <lehoannamkha2002@gmail.com>
2022-05-26 16:31:31 +0700

    Get list users, user by id and add user

---
+++ routes/api.js
@@ -0,0 +1,84 @@
+var express = require('express')
+const db = require('../database')
+var router = express.Router()
+var md5 = require("md5")
+
+// Get list of users
+router.get('/users', (req, res, next) => {
+    var sql = 'SELECT * FROM USER'
+    var params = []
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({
+                'error': err.message
+            })
+            return
+        }
+        res.json({
+            'message': 'Success',
+            'data': rows
+        })
+    })
+})
+
+// Get user by id
+router.get('/user/:id', (req, res, next) => {
+    var sql = 'SELECT * FROM USER WHERE id = ?'
+    var params = [req.params.id]
+
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({
+                'error': err.message
+            });
+            return;
+        }
+        res.json({
+            'message': 'Success',
+            'data': row
+        })
+    })
+})
+
+// Add user
+router.post('/user/', (req, res, next) => {
+    var errors = []
+
+    if (!req.body.password) {
+        errors.push('No password specified.')
+    }
+    if (!req.body.email) {
+        errors.push('No email specified.')
+    }
+    if (errors.length) {
+        res.status(400).json({
+            'error': errors.join(',')
+        })
+        return
+    }
+
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: md5(req.body.password)
+    }
+    var sql = 'INSERT INTO user (name, email, password) VALUES (?, ?, ?)'
+    var params = [data.name, data.email, data.password]
+
+    db.get(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({
+                'error': err.message
+            });
+            return;
+        }
+        res.json({
+            'message': 'Success',
+            'data': data,
+            'id': this.lastID
+        })
+    })
+})
+
+module.exports = router
