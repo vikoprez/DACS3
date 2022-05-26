844de9573cdb965fc18ff628301204f46fdd238c
vikopromotion <lehoannamkha2002@gmail.com>
2022-05-26 16:29:24 +0700

    Create database with only user

---
+++ database.js
@@ -0,0 +1,33 @@
+var sqlite3 = require('sqlite3').verbose()
+var md5 = require('md5')
+
+const DBSOURCE = 'db.sqlite'
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        // Can't open database
+        console.log(err.message)
+        throw err
+    } else {
+        console.log('Connected to the SQLite database.')
+        db.run(`CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            email text UNIQUE, 
+            password text, 
+            CONSTRAINT email_unique UNIQUE (email)
+        )`,
+        (err) => {
+            if (err) {
+                // Table already created
+            } else {
+                // Table created, add rows
+                var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+                db.run(insert, ['admin', 'admin@example.com', md5('admin123456')])
+                db.run(insert, ['user', 'user@example.com', md5('user123456')])
+            }
+        })
+    }
+})
+
+module.exports = db
