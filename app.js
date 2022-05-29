6d373b3f4eea378a783a05d9844932f5b298b4cd
vikopromotion <lehoannamkha2002@gmail.com>
2022-05-29 13:48:02 +0700

    Remove users direction

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use('/api', apiRouter)
 
 // catch 404 and forward to error handler
